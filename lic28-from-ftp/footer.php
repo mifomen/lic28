a3eaae9ec5a822c11d3c966241327d8d7ec83c92
Ilya Koshelev <mifomen@yandex.ru>
2020-08-28 13:51:25 +0300

    add pd bottom

--- lic28-from-ftp/footer.php
+++ lic28-from-ftp/footer.php
@@ -15,6 +15,11 @@
 	<?php wp_footer(); ?>
 	<!-- <?php printf(__('%d queries. %s seconds.', THEME_NS), get_num_queries(), timer_stop(0, 3)); ?> -->
 </div>
+<div class="cookie vh" id='cookie' style='display: none;'>
+    <span class="cookie__text">Сайт использует cookie, чтобы сделать пользование сайтом проще.</span> 
+    <button  type="button" id="cookie__button" class="cookie__button">Ok</button>
+</div>
+<script src='wp-content/themes/UNN-liceum28/main.js'></script>
 </body>
 </html>
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,64 @@
-console.log('mifomanasd sd')
+// console.log('mifomanasd sd')
 
-    var CurrentYear = new Date();
+//     var CurrentYear = new Date();
 
-document.querySelector('.CurrentYear').textContent=CurrentYear.getUTCFullYear() + ' г.'
+// document.querySelector('.CurrentYear').textContent=CurrentYear.getUTCFullYear() + ' г.'
+
+
+CookieButton = document.getElementById('cookie__button');
+
+CookieDiv = document.querySelector('.cookie');
+CookieButton.addEventListener('click',function () {
+  // console.log('click')
+  // document.CookieButton.disabled = true;
+  this.disabled = true;
+  CookieDiv.classList.add('vh');
+   CookieDiv.style.cssText='display: none;';
+  localStorage.CookieMemory = true;
+  // alert(localStorage.CookieMemory)
+
+})
+
+// CheckCookieMemory = function () {
+//    if (localStorage.CookieMemory) {
+//     CookieDiv.classList.add('vh')
+//     // CookieDiv.style.cssText=""
+//    // document.getElementById('cookie').style.cssText=""
+
+//   }
+// }
+// CheckCookieMemory();
+// fix = function () {
+//      document.getElementById('cookie').style.cssText=""
+// }
+
+document.addEventListener('DOMContentLoaded',function () {
+  proverka();
+
+})
+
+
+ClearCokieMemory = function () {
+  return delete localStorage.CookieMemory;
+}
+
+
+proverka = function () {
+  if (CookieDiv.classList.contains('vh') && localStorage.CookieMemory === undefined) {
+     CookieDiv.classList.remove('vh');
+     CookieDiv.style.cssText='';
+
+    // console.log('text') //true
+  } else {
+
+    if (CookieDiv.classList.contains('vh') || localStorage.CookieMemory) {
+      // CookieDiv.style.cssText='display: none;'
+      // CookieDiv.classList.add('vh'),
+    }
 
 
 
-// console.log(date)
-// date.value=getFullYear()
+  }
+}
 
-// var date = new Date();
-// console.log(date)
+proverka();
