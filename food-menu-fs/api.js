021ce9fb350feab1af8a7cf8f1ff4dc01be953af
Ilya Koshelev <mifomen@yandex.ru>
2022-03-11 16:55:33 +0300

    new fetch

--- food-menu-fs/api.js
+++ food-menu-fs/api.js
@@ -12,6 +12,36 @@ fetch(URL)
 console.log(getFileData(URL));
 
 
+async function getUsers(names) {
+  let jobs = [];
+
+  for(let name of names) {
+    let job = fetch(`https://api.github.com/users/${name}`).then(
+      successResponse => {
+        if (successResponse.status != 200) {
+          return null;
+        } else {
+          return successResponse.json();
+        }
+      },
+      failResponse => {
+        return null;
+      }
+    );
+    jobs.push(job);
+  }
+
+  let results = await Promise.all(jobs);
+
+  return results;
+}
+
+console.log(getUsers(URL));
+
+
+const nowTime = new Date();
+
+console.log(`текущий день месяца ${nowTime.getDate()})
 // const getData = () => {
 //   fetch(URL)
 //     .then((response) => {
