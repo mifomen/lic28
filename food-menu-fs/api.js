ecfc723bc7549994a4c0189ea9f8c47a1e5b00be
Ilya Koshelev <mifomen@yandex.ru>
2022-03-12 11:39:38 +0300

    changes for timemene food itesm on site pages

--- food-menu-fs/api.js
+++ food-menu-fs/api.js
@@ -2,69 +2,98 @@ const URL_FILE_ID = `05032022`;
 const URL = `https://liceum28.nnov.ru/files/menu-${URL_FILE_ID}.pdf`;
 
 const getFileData = (URL) => {
-fetch(URL)
-  // .then(res => res.ok ? true : false)
-  .then(res => res.ok ? res : Promise.reject(res))
-  .then(data => { console.log('+', data)})
-  .catch((e) => console.log(`some error ${e.message}`));
-}
+  fetch(URL)
+    // .then(res => res.ok ? true : false)
+    .then((res) => (res.ok ? res : Promise.reject(res)))
+    .then((data) => {
+      console.log("+", data);
+    })
+    .catch((e) => console.log(`some error ${e.message}`));
+};
 
 console.log(getFileData(URL));
-
 
 async function getUsers(names) {
   let jobs = [];
 
-  for(let name of names) {
-    let job = fetch(`https://api.github.com/users/${name}`).then(
-      successResponse => {
+  for (let name of names) {
+    let job = fetch(URL).then(
+      (successResponse) => {
         if (successResponse.status != 200) {
           return null;
         } else {
-          return successResponse.json();
+          return successResponse.blob();
         }
       },
-      failResponse => {
+      (failResponse) => {
         return null;
       }
     );
-    jobs.push(job);
+    jobs.push(URL);
   }
 
   let results = await Promise.all(jobs);
 
   return results;
 }
+const arrayMonths = ['января','февраля','марта','апреля','мая','июня','июля','августа','сентября','октября','ноября','декабря']
 
 console.log(getUsers(URL));
+const start = new Date('09/02/2021');
+console.log(start)
+const end = new Date();
 
+let i = 0;
+let loop = new Date(start);
+while(loop <= end){
+ i+=1;
 
-const nowTime = new Date();
+ let nowDate
+if (loop.getDate() < 10) {
+  nowDate = `0${loop.getDate()}${loop.getMonth()}${loop.getFullYear()}`;
+} else {
+  nowDate = `${loop.getDate()}${loop.getMonth()}${loop.getFullYear()}`;
+}
 
-console.log(`текущий день месяца ${nowTime.getDate()})
-// const getData = () => {
-//   fetch(URL)
-//     .then((response) => {
-//       if (response.status >= 200 && response.status < 300) {
-//         return true
-//       } else {
-//         let error = new Error(response.statusText);
-//         error.response = response;
-//         throw error;
-//       }
-//     })
-//     // .then((response) => {
-//     //   if (response.ok) {
-//     //     console.log(`file exist`);
-//     //   } else {
-//     //     console.log('Не удалось найти файл с питанием');
-//     //   }
-//     // })
-//     .catch((e) => {
-//       return false;
-//       console.log('error: ' + e.message);
-//       console.log(e.response);
-//     })
-// };
+const pathFile = `menu-${nowDate}.pdf`;
+console.log(`${pathFile}`)
 
-// console.log(getData(URL));
+   let newDate = loop.setDate(loop.getDate() + 1);
+   loop = new Date(newDate);
+}
+
+const spoilerForMenuFiles = document.querySelector('.js-food-menu');
+console.log(spoilerForMenuFiles);
+
+const createNewFileMenu = () => {
+  const newMenuFileItemLink = document.createElement('div')
+  newMenuFileItemLink.className='document__list';
+
+  const iconFile = document.createElement('img');
+  iconFile.src="https://www.liceum28.nnov.ru/files/pdf-icon.gif";
+  iconFile.width='52';
+  iconFile.alt='picture';
+  newMenuFileItemLink.appendChild(iconFile)
+
+  const menuFileContent = document.createElement('div');
+  menuFileContent.style.cssText = "color: #000;";
+  menuFileContent.className='document__content';
+
+  const menuFileLink = document.createElement('a');
+  menuFileLink.target="_blank";
+  menuFileLink.className = "document__lead";
+  menuFileLink.href="https://liceum28.nnov.ru/files/menu-11032022.pdf";
+  menuFileLink.rel="noopener";
+  menuFileLink.textContent = 'Школьнок питание 5-11 класс'
+  menuFileContent.appendChild(menuFileLink);
+
+  const menuFileLinkDescription = document.createElement('p');
+  menuFileLinkDescription.className = "document__description";
+  menuFileLinkDescription.style.cssText="font-size: 14px; font-weight: 500;";
+  menuFileLinkDescription.textContent = `Файл Adobe PDF, ~160 Кбайт. Дата размещения: 11 марта 2022`
+  menuFileContent.appendChild(menuFileLinkDescription);
+  newMenuFileItemLink.appendChild(menuFileContent)
+
+  spoilerForMenuFiles.appendChild(newMenuFileItemLink)
+}
+createNewFileMenu(); createNewFileMenu();
