89f88ff159d8a543fa0634bbc9387aad7d9cfe04
Ilya Koshelev <mifomen@yandex.ru>
2022-04-18 16:59:32 +0300

    fix

---
+++ src/chartsJS/app.js
@@ -0,0 +1,66 @@
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+      label: 'Ученики за год',
+      data: [65, 59, 80, 81, 26, 55, 40],
+      fill: false,
+      borderColor: 'rgb(75, 192, 192)',
+    }]},
+  type: 'line',
+  options: {
+    animations: {
+      tension: {
+        duration: 10000,
+        easing: 'linear',
+        from: 1,
+        to: 0,
+        loop: false
+      }
+    },
+    borderWidth: 15,
+    scales: {
+      y: { // defining min and max so hiding the dataset does not change scale range
+        min: 0,
+        max: 100
+      }
+    }
+  }
+});
+
+
+
+// const data = {
+//   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+//   datasets: [{
+//     label: 'Looping tension',
+//     data: [65, 59, 80, 81, 26, 55, 40],
+//     fill: false,
+//     borderColor: 'rgb(75, 192, 192)',
+//   }]
+// };
+
+
+// const config = {
+//   type: 'line',
+//   data: data,
+//   options: {
+//     animations: {
+//       tension: {
+//         duration: 1000,
+//         easing: 'linear',
+//         from: 1,
+//         to: 0,
+//         loop: true
+//       }
+//     },
+//     scales: {
+//       y: { // defining min and max so hiding the dataset does not change scale range
+//         min: 0,
+//         max: 100
+//       }
+//     }
+//   }
+// };
