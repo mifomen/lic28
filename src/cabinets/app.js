941933bfd4059d04bde40db9c051d0c78273819a
Ilya Koshelev <mifomen@yandex.ru>
2022-04-20 22:36:51 +0300

    final

--- src/cabinets/app.js
+++ src/cabinets/app.js
@@ -1,27 +1,33 @@
-const allItems = document.querySelectorAll('.list__item');
+// const allItems = document.querySelectorAll('.list__item');
 
-const onClickChangeActive = (evt) => {
-  if (document.querySelector('.list__item--active')) {
-    document.querySelector('.list__item--active').classList.remove('list__item--active');
-    evt.target.classList.add('list__item--active');
+// const onClickChangeActive = (evt) => {
+//   if (document.querySelector('.list__item--active')) {
+//     document.querySelector('.list__item--active').classList.remove('list__item--active');
+//     evt.target.classList.add('list__item--active');
 
-    const days = document.querySelectorAll('.day');
+//     const days = document.querySelectorAll('.day');
 
-    for (const day of days) {
-      if (!day.classList.contains('hidden')) {
-        day.classList.add('hidden');
-      }
-    }
+//     for (const day of days) {
+//       if (!day.classList.contains('hidden')) {
+//         day.classList.add('hidden');
+//       }
+//     }
 
-    const dayTitles = document.querySelectorAll('.day__title');
-    dayTitles.forEach( item => {
-      if (item.textContent === evt.target.textContent) {
-        return item.parentElement.classList.toggle('hidden');
-      }
-    })
-  }
-};
+//     const dayTitles = document.querySelectorAll('.day__title');
+//     dayTitles.forEach( item => {
+//       if (item.textContent === evt.target.textContent) {
+//         return item.parentElement.classList.toggle('hidden');
+//       }
+//     })
+//   }
+// };
 
-for (const item of allItems) {
-  item.addEventListener('click',onClickChangeActive);
+// for (const item of allItems) {
+//   item.addEventListener('click',onClickChangeActive);
+// }
+
+
+const url = `#day-${new Date().getDay()}`;
+if (document.querySelector(url)) {
+  document.querySelector(url).checked = true;
 }
