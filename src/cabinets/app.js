37edf26d3e49559382ad3450cdd2c2c16297214e
Ilya Koshelev <mifomen@yandex.ru>
2022-04-21 15:13:14 +0300

    fix

--- src/cabinets/app.js
+++ src/cabinets/app.js
@@ -1,7 +1,7 @@
 
 
 
-const url = `#day-${new Date().getDay()}`;
-if (document.querySelector(url)) {
-  document.querySelector(url).checked = true;
-}
+// const url = `#day-${new Date().getDay()}`;
+// if (document.querySelector(url)) {
+//   document.querySelector(url).checked = true;
+// }
