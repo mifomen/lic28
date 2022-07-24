9d45d8aca8efd90348f17fb724994516863005e4
Ilya Koshelev <mifomen@yandex.ru>
2022-07-24 13:49:25 +0300

    full

--- src/cabinets-vue3/index.js
+++ src/cabinets-vue3/index.js
@@ -14,17 +14,25 @@ createApp({
       fetch("./cabinets.json")
         .then((response) => response.json())
         .then((json) => this.cabinets = json)
-        .then((json) => console.log(json))
+        // .then((json) => console.log(json))
         .then(this.init = true);
     } catch(erros) {
         console.error("erros=", erros);
       }
     },
   computed: {
-    resaultArray: function() {
-      const res = new Array(5);
-
-      return res;
+    getResaultArray: function () {
+      for (const item of this.cabinets) {
+        const itemArrays = new Array(5);
+        itemArrays[0] = item.DayOfWeek1;
+        itemArrays[1] = item.DayOfWeek2;
+        itemArrays[2] = item.DayOfWeek3;
+        itemArrays[3] = item.DayOfWeek4;
+        itemArrays[4] = item.DayOfWeek5;
+        itemArrays[5] = item.DayOfWeek6;
+        this.cabinets.data = itemArrays;
+        // return itemArrays;
+      }
     },
   },
 }).mount('#app');
