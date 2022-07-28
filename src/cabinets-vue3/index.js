ab240976fb86a145dd40f990c560d924433702c2
Ilya Koshelev <mifomen@yandex.ru>
2022-07-28 21:50:04 +0300

    did apge update on click and data for second shift

--- src/cabinets-vue3/index.js
+++ src/cabinets-vue3/index.js
@@ -8,41 +8,27 @@ createApp({
       init: false,
       cabinets: [],
       itemArray1: [],
+      pageNumber: new Date().getDay(),
+      arraySecondShift: ['6А','6Б','6В','7А','7Б','7В'],
+      arrayNotSecondShift: [],
     };
   },
   methods: {
     backArray(evt) {
-
-      console.log(evt.target.id); //eslint-disable-line
+      this.pageNumber = evt.target.id;
       if (document.querySelector('.button--active')) {
         document.querySelector('.button--active').classList.remove('button--active');
       }
       evt.target.classList.add('button--active');
 
 
-      console.log("itemArray1",this.itemArray1); //eslint-disable-line
-
-      if (evt.target.id === 0) {
-        this.itemArray = this.cabinet[0].DayOfWeek1;
-      }
-      if (evt.target.id === 1) {
-        this.itemArray = this.cabinet[0].DayOfWeek2;
-      }
-      if (evt.target.id === 2) {
-        this.itemArray = this.cabinet[0].DayOfWeek3;
-      }
-      if (evt.target.id === 3) {
-        this.itemArray = this.cabinet[0].DayOfWeek4;
-      }
-      if (evt.target.id === 4) {
-        this.itemArray =  this.cabinet[0].DayOfWeek5;
-      }
-      if (evt.target.id === 5) {
-        this.itemArray = this.cabinet[0].DayOfWeek6;
-      }
     },
   },
   async mounted() {
+    if (document.querySelector('.button--active')) {
+      document.querySelector('.button--active').classList.remove('button--active');
+    }
+    document.getElementById(this.pageNumber).classList.add('button--active');
     try {
       await fetch('./cabinets.json')
         .then((response) => response.json())
@@ -54,7 +40,38 @@ createApp({
     }
   },
   computed: {
-    getResaultArray: function () {
+    getResaultArrayNotSecondShift: function () {
+      const itemArrays1 = new Array(5);
+      const templateArrays0 = new Array();
+      const templateArrays1 = new Array();
+      const templateArrays2 = new Array();
+      const templateArrays3 = new Array();
+      const templateArrays4 = new Array();
+      const templateArrays5 = new Array();
+      // const templateArraysNames = new Array();
+      for (const item of this.cabinets) {
+        if (item.secondShift ) {
+          templateArrays0.push(item.DayOfWeek0);
+          templateArrays1.push(item.DayOfWeek1);
+          templateArrays2.push(item.DayOfWeek2);
+          templateArrays3.push(item.DayOfWeek3);
+          templateArrays4.push(item.DayOfWeek4);
+          templateArrays5.push(item.DayOfWeek5);
+          // templateArraysNames.push(item.name);
+        }
+      }
+      itemArrays1[0] = templateArrays0;
+      itemArrays1[1] = templateArrays1;
+      itemArrays1[2] = templateArrays2;
+      itemArrays1[3] = templateArrays3;
+      itemArrays1[4] = templateArrays4;
+      itemArrays1[5] = templateArrays5;
+      // itemArrays1[6] = templateArraysNames;
+      // this.cabinets.data = itemArrays1;
+      return itemArrays1;
+    },
+
+    getResaultArraySecondShift: function () {
       const itemArrays = new Array(5);
       const templateArrays0 = new Array();
       const templateArrays1 = new Array();
@@ -62,6 +79,7 @@ createApp({
       const templateArrays3 = new Array();
       const templateArrays4 = new Array();
       const templateArrays5 = new Array();
+      // const templateArraysNames = new Array();
       for (const item of this.cabinets) {
         if (!item.secondShift ) {
           templateArrays0.push(item.DayOfWeek0);
@@ -70,6 +88,7 @@ createApp({
           templateArrays3.push(item.DayOfWeek3);
           templateArrays4.push(item.DayOfWeek4);
           templateArrays5.push(item.DayOfWeek5);
+          // templateArraysNames.push(item.name);
         }
       }
       itemArrays[0] = templateArrays0;
@@ -78,6 +97,8 @@ createApp({
       itemArrays[3] = templateArrays3;
       itemArrays[4] = templateArrays4;
       itemArrays[5] = templateArrays5;
+      // itemArrays[6] = templateArraysNames;
+      // this.cabinets.data = itemArrays;
       return itemArrays;
     },
     // getWeek1: function () {
