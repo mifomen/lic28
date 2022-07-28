26cd8c54513dce39d156640b3d831cc164407270
Ilya Koshelev <mifomen@yandex.ru>
2022-07-28 22:23:41 +0300

    final, w8 data set in json

--- src/cabinets-vue3/index.js
+++ src/cabinets-vue3/index.js
@@ -9,8 +9,7 @@ createApp({
       cabinets: [],
       itemArray1: [],
       pageNumber: new Date().getDay(),
-      arraySecondShift: ['6А','6Б','6В','7А','7Б','7В'],
-      arrayNotSecondShift: [],
+      itemCount: 6,
     };
   },
   methods: {
@@ -31,82 +30,20 @@ createApp({
     document.getElementById(this.pageNumber).classList.add('button--active');
     try {
       await fetch('./cabinets.json')
-        .then((response) => response.json())
-        .then((json) => this.cabinets = json)
-        // .then((json) => console.log(json))
-        .then(this.init = true);
+      .then((response) => response.json())
+      .then((json) => this.cabinets = json)
+      // .then((json) => console.log(json))
+      .then(this.init = true);
     } catch(erros) {
       console.error('erros=', erros); //eslint-disable-line
     }
+    this.itemCount = this.cabinets[this.cabinets.length-1].data[0].length; // считаем сколько раз вывести индекс строки
   },
   computed: {
     getResaultArrayNotSecondShift: function () {
       const itemArrays1 = new Array(5);
-      const templateArrays0 = new Array();
-      const templateArrays1 = new Array();
-      const templateArrays2 = new Array();
-      const templateArrays3 = new Array();
-      const templateArrays4 = new Array();
-      const templateArrays5 = new Array();
-      // const templateArraysNames = new Array();
-      for (const item of this.cabinets) {
-        if (item.secondShift ) {
-          templateArrays0.push(item.DayOfWeek0);
-          templateArrays1.push(item.DayOfWeek1);
-          templateArrays2.push(item.DayOfWeek2);
-          templateArrays3.push(item.DayOfWeek3);
-          templateArrays4.push(item.DayOfWeek4);
-          templateArrays5.push(item.DayOfWeek5);
-          // templateArraysNames.push(item.name);
-        }
-      }
-      itemArrays1[0] = templateArrays0;
-      itemArrays1[1] = templateArrays1;
-      itemArrays1[2] = templateArrays2;
-      itemArrays1[3] = templateArrays3;
-      itemArrays1[4] = templateArrays4;
-      itemArrays1[5] = templateArrays5;
-      // itemArrays1[6] = templateArraysNames;
-      // this.cabinets.data = itemArrays1;
-      return itemArrays1;
+      return true;
     },
-
-    getResaultArraySecondShift: function () {
-      const itemArrays = new Array(5);
-      const templateArrays0 = new Array();
-      const templateArrays1 = new Array();
-      const templateArrays2 = new Array();
-      const templateArrays3 = new Array();
-      const templateArrays4 = new Array();
-      const templateArrays5 = new Array();
-      // const templateArraysNames = new Array();
-      for (const item of this.cabinets) {
-        if (!item.secondShift ) {
-          templateArrays0.push(item.DayOfWeek0);
-          templateArrays1.push(item.DayOfWeek1);
-          templateArrays2.push(item.DayOfWeek2);
-          templateArrays3.push(item.DayOfWeek3);
-          templateArrays4.push(item.DayOfWeek4);
-          templateArrays5.push(item.DayOfWeek5);
-          // templateArraysNames.push(item.name);
-        }
-      }
-      itemArrays[0] = templateArrays0;
-      itemArrays[1] = templateArrays1;
-      itemArrays[2] = templateArrays2;
-      itemArrays[3] = templateArrays3;
-      itemArrays[4] = templateArrays4;
-      itemArrays[5] = templateArrays5;
-      // itemArrays[6] = templateArraysNames;
-      // this.cabinets.data = itemArrays;
-      return itemArrays;
-    },
-    // getWeek1: function () {
-    //   this.cabinet.forEach((item) => {
-    //     this.itemArray1.push(item.DayOfWeek1);
-    //   });
-    //   console.log("itemArray1",this.itemArray1); //eslint-disable-line
-    // },
   },
 }).mount('#app');
 
