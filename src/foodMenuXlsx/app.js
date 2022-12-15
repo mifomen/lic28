05f3be1c8ae1a09d92a6b5786af37446772dfdf3
Ilya Koshelev <mifomen@yandex.ru>
2022-12-15 15:25:19 +0300

    Master

--- src/foodMenuXlsx/app.js
+++ src/foodMenuXlsx/app.js
@@ -27,10 +27,6 @@ if (document.querySelector('.js-food-menu-high') && document.querySelector('.js-
 
   const arrayMonths = ['января','февраля','марта','апреля','мая','июня','июля','августа','сентября','октября','ноября','декабря'];
 
-  const arrayDaysNoFoodLowSchool = ['04092021','11092021','18092021','25092021','02102021','09102021','16102021','23102021','30102021','06112021','13112021','20112021','27112021','04122021','11122021','18122021','25122021','15012022','22012022','29012022','05022022','12022022','19022022','26022022','12032022','19032022','26032022','02042022','09042022','16042022','23042022','30042022','07052022','21052022','28052022'];
-
-  const arrayDaysNoFoodHighSchool = ['05092021','12092021','19092021','26092021','03102021','10102021','17102021','24102021','31102021','07112021','14112021','21112021','28112021','05122021','12122021','19122021','26122021','01012022','02012022','03012022','04012022','05012022','06012022','07012022','08012022','09012022','16012022','2312022','30012022','06022022','13022022','20022022','27022022','06032022','13032022','20032022','27032022','03042022','10042022','17042022','24042022','01052022','08052022','15052022','22052022','29052022','30052022'];
-
   const spoilerForMenuFilesHigh = document.querySelector('.js-food-menu-high');
   const spoilerForMenuFilesLow = document.querySelector('.js-food-menu-low');
 
@@ -49,7 +45,7 @@ if (document.querySelector('.js-food-menu-high') && document.querySelector('.js-
     newMenuFileItemLink.className = 'document__list';
 
     const iconFile = document.createElement('img');
-    iconFile.src = 'https://www.liceum28.nnov.ru/files/pdf-icon.gif';
+    iconFile.src = 'https://www.liceum28.nnov.ru/files/xlsx-icon.gif';
     iconFile.width = '52';
     iconFile.alt = 'picture';
     newMenuFileItemLink.appendChild(iconFile);
@@ -61,19 +57,24 @@ if (document.querySelector('.js-food-menu-high') && document.querySelector('.js-
     const menuFileLink = document.createElement('a');
     menuFileLink.target = '_blank';
     menuFileLink.className = 'document__lead';
-    menuFileLink.href = `https://liceum28.nnov.ru/files/menu-${urlFile}.pdf`;
+    // https://www.liceum28.nnov.ru/food/2022-12-02-sm.xlsx
+    menuFileLink.href = `https://liceum28.nnov.ru/food/${urlFile}.xlsx`;
     menuFileLink.rel = 'noopener';
 
     const menuFileLinkDescription = document.createElement('p');
     menuFileLinkDescription.className = 'document__description';
     menuFileLinkDescription.style.cssText = 'font-size: 14px; font-weight: 500;';
 
-    if (urlFile.length === 9) {
-      menuFileLink.textContent = 'Школьнок питание 1-4 класс'; //menu009032022.pdf началка
-      menuFileLinkDescription.textContent = `Файл Adobe PDF, ~160 Кбайт. Дата размещения: ${parseInt(urlFile.slice(1, 3),10)} ${arrayMonths[parseInt(urlFile.slice(3, 5), 10) - 1]} ${urlFile.slice(urlFile.length - 4,urlFile.length)}`;
-    } else {
-      menuFileLink.textContent = 'Школьнок питание 5-11 класс'; //menu09032022.pdf среднее звено
-      menuFileLinkDescription.textContent = `Файл Adobe PDF, ~160 Кбайт. Дата размещения: ${parseInt(urlFile.slice(0, 2),10)} ${arrayMonths[parseInt(urlFile.slice(2, 4), 10) - 1]} ${urlFile.slice(urlFile.length - 4,urlFile.length)}`;
+    if (levelEducation===2) {
+      menuFileLink.textContent = 'Школьное питание 1-4 класс'; //menu009032022.pdf началка
+      menuFileLinkDescription.textContent = `Файл XLS , ~23 Кбайт. Дата размещения:   ${parseInt(urlFile.slice(urlFile.length-5,urlFile.length-3),10)} ${arrayMonths[parseInt(urlFile.slice(5,7), 10)-1]} ${parseInt(urlFile.slice(0,4),10)}`;
+      // console.log('arrayMonths[parseInt(urlFile.slice(5, 7), 10) ',parseInt(urlFile.slice(1, 5), 10))
+    }
+
+    if (levelEducation===1) {
+      menuFileLink.textContent = 'Школьное питание 5-11 класс'; //menu009032022.pdf началка
+      menuFileLinkDescription.textContent = `Файл XLS , ~23 Кбайт. Дата размещения:   ${parseInt(urlFile.slice(urlFile.length-5,urlFile.length-3),10)} ${arrayMonths[parseInt(urlFile.slice(5,7), 10)-1]} ${parseInt(urlFile.slice(0,4),10)}`;
+      console.log('arrayMonths[parseInt(urlFile.slice(5, 7), 10) ',parseInt(urlFile.slice(1, 5), 10))
     }
     console.log("urlFile ",urlFile)
 
@@ -105,7 +106,7 @@ if (document.querySelector('.js-food-menu-high') && document.querySelector('.js-
     let end;
     let start;
     start = new Date('09-01-2022');
-    end = new Date('11-01-2022');
+    end = new Date('01-01-2023');
     // if (spoilerForMenuFilesLow.hasAttribute('data-food-start')) {
     //   start = new Date(spoilerForMenuFilesLow.dataset.foodStart);
     // } else {
@@ -124,43 +125,43 @@ if (document.querySelector('.js-food-menu-high') && document.querySelector('.js-
 
 
   while (loop >= start) {
-
-
+    // console.log(`loop = ${loop}`)
     let nowDate;
-    if (loop.getDate() < 10 && loop.getMonth() > 9) {
-      nowDate = `0${loop.getDate()}${loop.getMonth() + 1}${loop.getFullYear()}`;
-    } else {
-      if (loop.getDate() < 10 && loop.getMonth() < 10) {
-        nowDate = `0${loop.getDate()}0${
-          loop.getMonth() + 1
-        }${loop.getFullYear()}`;
-      } else {
-        if (loop.getMonth() < 10 && loop.getDate() > 9) {
-          nowDate = `${loop.getDate()}0${
-            loop.getMonth() + 1
-          }${loop.getFullYear()}`;
-        } else {
-          nowDate = `${loop.getDate()}${
-            loop.getMonth() + 1
-          }${loop.getFullYear()}`;
-        }
-      }
-    }
+    let templateDate;
+    let templateMonth;
+
+    // console.log(`year = ${loop.getYear()}`)
+     if (loop.getDate() < 10) {
+       templateDate = `0${loop.getDate()}`
+     }
+     else {
+       templateDate = loop.getDate();
+     }
+
+     if (loop.getMonth() < 10) {
+       templateMonth= `0${loop.getMonth()}`;
+     }
+     else {
+      templateMonth= `${loop.getMonth()}`
+     }
+     nowDate = `${loop.getFullYear()}-${templateMonth}-${templateDate}`;
+
 
     const settingsOfFetch = {
       mode: 'cors', // no-cors, *cors, same-origin
       cache: 'no-cache',
     };
-    // console.log("loop=",loop.getDay())
-    if (loop.getDay() != 6 || arrayDaysNoFoodLowSchool.indexOf(`${nowDate}`) === -1 && arrayDaysNoFoodHighSchool.indexOf(nowDate) === -1 ) {
-      const pathFullHighSchool = `https://www.liceum28.nnov.ru/wp-includes/ms-files.php?file=menu-${nowDate}.pdf`;
-
+    console.log("loop=",loop.getDay())
+    if (loop.getDay() != 6 ) {
+      // const pathFullHighSchool = `https://www.liceum28.nnov.ru/wp-includes/ms-files.php?file=menu-${nowDate}.pdf`;
+      const pathFullHighSchool = `https://www.liceum28.nnov.ru/food/${nowDate}-ss.xlsx`;
+      // console.log('pathFullHighSchool ',pathFullHighSchool)
       fetch(pathFullHighSchool, settingsOfFetch).then(
         (successResponse) => {
           if (successResponse.status !== 200) {
             return null;
           } else {
-            createNewFileMenu(nowDate, 1);
+            createNewFileMenu(`${nowDate}-ss`, 1);
             return successResponse.formData();
           }
         }, //eslint-disable-next-line
@@ -170,15 +171,15 @@ if (document.querySelector('.js-food-menu-high') && document.querySelector('.js-
       );
     }
 
-    if ( loop.getDay() != 6 || loop.getDay() != 5 || arrayDaysNoFoodLowSchool.indexOf(`${nowDate}`) === -1 || arrayDaysNoFoodHighSchool.indexOf(nowDate) === -1) {
-      const pathFullLowSchool = `https://www.liceum28.nnov.ru/wp-includes/ms-files.php?file=menu-0${nowDate}.pdf`;
-
+    if ( loop.getDay() != 6 || loop.getDay() != 5 ) {
+      // const pathFullLowSchool = `https://www.liceum28.nnov.ru/wp-includes/ms-files.php?file=menu-0${nowDate}.pdf`;
+      const pathFullLowSchool = `https://www.liceum28.nnov.ru/food/${nowDate}-sm.xlsx`;
       fetch(pathFullLowSchool, settingsOfFetch).then(
         (successResponse) => {
           if (successResponse.status !== 200) {
             return null;
           } else {
-            createNewFileMenu(`0${nowDate}`, 2);
+            createNewFileMenu(`${nowDate}-sm`, 2);
             return successResponse.formData();
           }
         }, //eslint-disable-next-line
