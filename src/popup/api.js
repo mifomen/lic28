3e1e3bfe82aa4eba6216dadfbdccfa66af4beb94
Ilya Koshelev <mifomen@yandex.ru>
2023-10-03 22:18:51 +0300

    fix

--- src/popup/api.js
+++ src/popup/api.js
@@ -3,7 +3,36 @@ const popupBtn = document.querySelectorAll('.js-info-link')
 const popup = document.querySelector('.js-info-popup')
 const popupInformater = document.querySelector('.js-info-popup-info')
 
+const newsBtns = document.querySelectorAll('.menu-news__link');
+newsBtns.forEach(item => {
+  item.addEventListener('click', (evt) => {
+    evt.preventDefault()
+    const contentInput = document.querySelector('.js-news-content')
+    contentInput.classList.toggle('vh')
+    const choosen = evt.target;
 
+    if (choosen.classList.contains('menu-news__item-1')) {
+      document.querySelector('.js-news-content').innerHTML=document.querySelector('.menu-news__item-content-1').innerHTML;
+    }
+    if (choosen.classList.contains('menu-news__item-2')) {
+      document.querySelector('.js-news-content').innerHTML=document.querySelector('.menu-news__item-content-2').innerHTML;
+    }
+    if (choosen.classList.contains('menu-news__item-3')) {
+      document.querySelector('.js-news-content').innerHTML=document.querySelector('.menu-news__item-content-3').innerHTML;
+    }
+
+    if (choosen.classList.contains('menu-news__item-4')) {
+      if (!document.querySelector('.js-news-content').classList.contains('vh')) {
+        document.querySelector('.js-news-content').innerHTML=document.querySelector('.menu-news__item-content-4').innerHTML;
+      } else {
+      document.querySelector('.js-news-content').innerHTML=document.querySelector('.menu-news__item-content-4').innerHTML;}
+    }
+
+    if (choosen.classList.contains('menu-news__item-5')) {
+      document.querySelector('.js-news-content').innerHTML=document.querySelector('.menu-news__item-content-5').innerHTML;
+    }
+  })
+})
 
 const showPopup2 = (evt) => {
   evt.preventDefault()
@@ -19,7 +48,7 @@ const showPopup = (evt) => {
   popupInformater.classList.toggle('popup-active')
   // evt.target.innerHTML="";
   popupInformater.innerHTML = evt.target.innerHTML;
-  document.body.classList.toggle('shadow')
+  document.body.classList.toggle('overlay')
 }
 
 // popup.addEventListener('click',showPopup)
@@ -28,4 +57,4 @@ popupBtn.forEach(item => {
   item.addEventListener('click',showPopup)
 })
 
-popupInformater.addEventListener('click',showPopup2)
+// popupInformater.addEventListener('click',showPopup2)
