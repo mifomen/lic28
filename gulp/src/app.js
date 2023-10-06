624da195a06365df62d66b655de5377131911d52
Ilya Koshelev <mifomen@yandex.ru>
2023-10-06 12:43:09 +0300

    fix

--- gulp/src/app.js
+++ gulp/src/app.js
@@ -1,2 +1,74 @@
-console.log("test123")
-// alert('123')
+
+
+const clearBR = (selector) => {
+  if (document.querySelector(selector)) {
+    const brInGrid = document.querySelectorAll(selector);
+    for (const item of brInGrid) {
+      item.remove();
+    }
+  }
+}
+
+clearBR('.clear-br br')
+
+
+clearBR('.menu-news__list br');
+
+
+
+if (document.querySelector('.menu-news__item-content') && document.querySelector('.menu-news__item-content'))  {
+
+
+
+  const frameContent = document.querySelector('.js-news-content');
+  const allContent = document.querySelectorAll('.menu-news__item-content')
+
+  const allContentArray = new Array();
+  allContent.forEach(item => {
+    allContentArray.push(item)
+  })
+
+  // console.log(`allContentArray=${allContentArray.dataset.content}`)
+
+  const allContentBtns = document.querySelectorAll('.js-link-content')
+
+  const findItemById = function (items, resolve) {
+    return items.find(item => item.dataset.content === resolve.dataset.link);
+  };
+
+  let choosen;
+  let showStatusFrame = false;
+
+  allContentBtns.forEach(item => {
+    item.addEventListener('click', (evt) => {
+      if (evt.target.hasAttribute('data-link') && evt.target.dataset.link != undefined && evt.target.dataset.link != "" ) {
+
+        clearBR('.menu-news__item-content br');
+      if (choosen === evt.target.dataset.link) {
+        frameContent.classList.toggle('vh')
+        showStatusFrame = false;
+      } else {
+        frameContent.classList.remove('vh')
+        showStatusFrame = true;
+      }
+      choosen = evt.target.dataset.link;
+
+
+        // console.log('has')
+        evt.preventDefault();
+        const elem = findItemById(allContentArray,evt.target)
+        frameContent.innerHTML = elem.innerHTML;
+      }
+  })
+  })
+
+
+  document.addEventListener('keypress', event => {
+    console.log(event.key)
+    if (event.key === "Escape" || event.key === "Enter" ) {
+      frameContent.classList.add('vh')
+    }
+  })
+
+
+  }
